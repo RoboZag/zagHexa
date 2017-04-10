695aefeeff3aa0955fb61da548f6083938d14a57
mahmoudElsayed0 <mahmoud.elsyed21@gmail.com>
2017-04-10 22:46:16 +0200

    Add ROS Code after debug

--- library/Main.cpp
+++ library/Main.cpp
@@ -1,63 +1,64 @@
-#include <Walking_Patterns.h>
+#include <std_msgs/Int8.h>
 #include <ros/ros.h>
-#include <std_msgs/String.h>
+#include <Walking_Patterns.h>
 
 char data;
 int mode = 1; //mode 1 for translation, mode 2 for tending, mode 3 for up and down
-Walking_Patterns walking = Walking_Patterns()
-void oncall(const std_msgs::String::ConstPtr& msg)
+Walking_Patterns walking = Walking_Patterns();
+void oncall(const std_msgs::Int8 msg)
 {
-	if (data == '1')
+	if (msg.data == 1)
 	{
-		if (mode == 1)
+		if (mode == 1){
 			walking.tripod_forward();
-		if (mode == 2)
-			walking.tilt_forward();
+			ROS_INFO(" The direction is [%d] ", msg.data);
+			}
+		//if (mode == 2)
+			//walking.tilt_forward();
 		if (mode == 3)
 			walking.standup();
 	}
-	if (data == '2')
+	if (msg.data == 2)
 	{
-		if (mode == 1)
+		if (mode == 1){
 			walking.tripod_left();
-		if (mode == 2)
-			walking.tilt_left();
-		//if (mode == 3)
-			//do something 
+			ROS_INFO(" The direction is [%d] ", msg.data);
+}
+		//if (mode == 2)
+			//walking.tilt_left();
+	//	if (mode == 3) //do something 
 	}
-	if (data == '3')
+	if (msg.data == 3)
 	{
 		if (mode == 1)
-			walking.tripod_backward();
-		if (mode == 2)
-			walking.tilt_backward();
+			walking.reset_angles();
+		//if (mode == 2)
+		//	walking.tilt_backward();
 		if (mode == 3)
 			walking.sitdown();
 	}
-	if (data == '4')
+	if (msg.data == '4')
 	{
 		if (mode == 1)
 			walking.tripod_right();
-		if (mode == 2)
-			walking.tilt_right();
-		//if (mode == 3)
-			// do something
+//		if (mode == 2)
+		//	walking.tilt_right();
+	//	if (mode == 3)		// do something
 	}
-	/*if (data == 'x')
-	{
+	//if (msg.data == 'x')
+//	{
 		// When x is call
-	}
-	if (data == 't')
-	{
+	//}
+//	if (msg.data == 't')
+	//{
 		// When t is call
-	}*/
-	if (data == 'c')
+//	}
+	if (msg.data == 'c')
 		mode++;
-	if (data == 's')
+	if (msg.data == 's')
 		walking.reset_angles();
 	if (mode > 3)
 		mode = 1;
-	ROS.INFO("data = [%s]", msg-> data.c_str());
 }
 
 void start()
