8cf5033064c93e9db7be42ea10cdab6d1727df5d
KhaledRisha <khaledrisha@gmail.com>
2017-07-16 01:28:06 +0200

    edits and optimizations

--- ArduinoCode/Sonar/ZagHexa_Sonar_ino/ZagHexa_Sonar_ino.ino
+++ ArduinoCode/Sonar/ZagHexa_Sonar_ino/ZagHexa_Sonar_ino.ino
@@ -1,9 +1,10 @@
 #include <Servo.h>
 
 // Defines Tirg and Echo pins of the Ultrasonic
-#define trigPin 4
-#define echoPin 5
-#define pirSensor 7  //set PIR pin
+#define trigPin 7
+#define echoPin 8
+#define volt 2
+#define pirSensor 10  //set PIR pin
 
 // Variables for the duration and the distance
 long duration;
@@ -14,11 +15,14 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   pinMode(pirSensor, INPUT);
+  pinMode(volt,OUTPUT);
   Serial.begin(9600);
-  myServo.attach(12); // Defines on which pin is the servo motor attached
+  myServo.attach(9); // Defines on which pin is the servo motor attached
 }
 void loop() {
- int sensorValue = digitalRead(pirSensor);
+  digitalWrite(2, HIGH); 
+  int sensorValue = digitalRead(pirSensor);
+ 
   // rotates the servo motor from 0 to 180 degrees
   for(int i=0;i<=180;i++){  
    if (sensorValue == 1) {
@@ -29,10 +33,9 @@ void loop() {
     Serial.print(2);
     Serial.print("-");
     }
-  delay(30);
-  myServo.write(i);
-  delay(30);
-  distance = calculateDistance(); // Calculating the distance by the ultrasonic for each degree.
+   myServo.write(i);
+   delay(30);
+   distance = calculateDistance(); // Calculating the distance by the ultrasonic for each degree.
   
   Serial.print(i); // Sends the current degree into the Serial Port
   Serial.print(","); // Sending this character will help me in indexing
@@ -43,28 +46,27 @@ void loop() {
   // Repeats the previous lines from 180 to 0 degrees
   for(int i=180;i>0;i--){  
     if (sensorValue == 1) {
-    Serial.print(0);
+    Serial.print(1);
     Serial.print("-");
   }
    else { 
-    Serial.print(1);
+    Serial.print(2);
     Serial.print("-");
     }
-  delay(30);
-  myServo.write(i);
-  delay(30);
-  distance = calculateDistance();
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(distance);
-  Serial.print(".");
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
   }
 }
 
 // Function for calculating the distance by the Ultrasonic
 int calculateDistance(){ 
   digitalWrite(trigPin, LOW); 
-  delayMicroseconds(10);
+  delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH); 
   delayMicroseconds(10);
@@ -73,3 +75,4 @@ int calculateDistance(){
   distance= duration*0.034/2;
   return distance;
 }
+
